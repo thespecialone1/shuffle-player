c475ec37e9002263b5dcf7d31eaf28ee53227467
thespecialone1 <s3neo36@gmail.com>
2026-06-11 19:20:42 +0400

    chore: configure vite to run on port 80

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 80,
+    host: true
+  },
+  preview: {
+    port: 80,
+    host: true
+  },
   plugins: [
     react(),
     tailwindcss(),
